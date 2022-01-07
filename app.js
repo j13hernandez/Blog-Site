5daf33c0ffbacfb33cfbfb00841bffdff0f034b1
Palma <1hernandezj3@gmail.com>
2022-01-07 12:44:19 -0500

    "Connect blogsite to MongoDB to store blog posts"

--- app.js
+++ app.js
@@ -2,7 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
 const app = express();
+
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 3000;
+}
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(
+  'mongodb+srv://palma:pubPass@cluster0.zdv0y.mongodb.net/blogDB?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
 
 const homeStartingContent =
   'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
@@ -11,46 +37,58 @@ const aboutContent =
 const contactContent =
   'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
+const defaultItems = [homeStartingContent, aboutContent, contactContent];
+
 const posts = [];
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
-  res.render('home', { startingContent: homeStartingContent, posts: posts });
+  Blog.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('home', {
+        startingContent: homeStartingContent,
+        posts: blogs,
+      });
+    }
+  });
 });
+
 app.get('/about', (req, res) => {
   res.render('about', { startingContent: aboutContent });
 });
+
 app.get('/contact', (req, res) => {
   res.render('contact', { startingContent: contactContent });
 });
+
 app.get('/compose', (req, res) => {
   res.render('compose');
 });
+
 app.get('/posts/:post', (req, res) => {
-  posts.forEach((element) => {
-    if (_.lowerCase(element.title) === _.lowerCase(req.params.post)) {
-      res.render('post', { postTitle: element.title, postContent: element.content });
+  const requestedTitle = _.lowerCase(req.params.post);
+  Blog.findOne({ title: requestedTitle }, (err, blog) => {
+    if (err) {
+      console.log(err);
     } else {
-      console.log('sike!');
-      console.log(_.lowerCase(req.params.post), _.lowerCase(element.title));
+      res.render('post', {
+        postTitle: blog.title,
+        postContent: blog.content,
+      });
     }
   });
 });
 
 app.post('/compose', (req, res) => {
-  const post = {
+  const post = new Blog({
     title: req.body.blogTitle,
     content: req.body.blogPost,
-  };
-  posts.push(post);
-
+  });
+  post.save();
   res.redirect('/');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
